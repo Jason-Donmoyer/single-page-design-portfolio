3574c7b26e0508d216d8acf0f571e4e6511626d5
Jason-Donmoyer <j.donmoyer523@gmail.com>
2022-08-13 10:04:00 -0400

    HTML, CSS, JS: -HTML: --added classes for slides -CSS: --added images to slide classes -JS: -started slideshow functionality -ISSUE: cannot get transition to work past firt click

---
+++ main.js
@@ -0,0 +1,49 @@
+// VARIABLES
+
+// Slideshow Buttons
+const backBtn = document.getElementById('back-btn');
+const fwdBtn = document.getElementById('fwd-btn');
+
+// Slides Container
+const slidesContainer = document.getElementById('slides-container');
+
+// Slides
+const slide1 = document.getElementById('slide-1');
+const slide2 = document.getElementById('slide-2');
+const slide3 = document.getElementById('slide-3');
+const slide4 = document.getElementById('slide-4');
+const slide5 = document.getElementById('slide-5');
+
+let slidesArr = [slide1, slide2, slide3, slide4, slide5];
+
+
+
+// Event Listeners to Change Slides
+
+let amount = 20;
+let initial = 0;
+// Back Button
+backBtn.addEventListener('click', () => {
+  // slidesContainer.style.transform = 'translate3d(20%, 0, 0)';
+  // slidesContainer.style.transformOrigin += '20%';
+  let lastEle = slidesArr.pop();
+  slidesArr.unshift(lastEle);
+  slidesArr.forEach(e => slidesContainer.appendChild(e));
+  // requestAnimationFrame(() => slidesContainer.style.transform = 'translate3d(20%, 0, 0)');
+  // slidesContainer.style.transform = 'translate3d(20%, 0, 0)';
+  // initial += amount;
+});
+
+
+
+// backBtn.addEventListener('transitionrun', () => {
+//   slidesContainer.style.transform = 'translate3d(20%, 0, 0)';
+// } );
+
+// Forward Button
+fwdBtn.addEventListener('click', () => {
+  // slidesContainer.style.transform = 'translate3d(-20%, 0, 0)';
+  let lastEle = slidesArr.shift();
+  slidesArr.push(lastEle);
+  slidesArr.forEach(e => slidesContainer.appendChild(e));
+});
